2c709f1f82f85331a74994abfb90ad035a8de0ab
abdellahkh <a.khouya@enim.ac.ma>
2022-02-04 17:44:06 +0100

    First try

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="Barre">
+      <ul>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">Competences</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+ul {
+  display: flex;
+  font-size: 15px;
+  background-color: red;
+}
+li {
+  padding: 0 30px;
+  line-height: 50px;
+}
+</style>
